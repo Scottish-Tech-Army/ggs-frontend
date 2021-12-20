aa75faca38f95e7595552af41a2d8bd19c0c7930
snowyPigeon <snoiwypigeon@yahoo.com>
2021-12-20 17:10:23 +0000

    Added closing X svg

--- src/App.js
+++ src/App.js
@@ -142,14 +142,28 @@ export default function App(props) {
         centered
         scrollable
       >
-        <Modal.Header></Modal.Header>
+        <Modal.Header>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-x-square closing-square"
+            viewBox="0 0 16 16"
+          >
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </Modal.Header>
         <Modal.Body>
-        <div className="place-name">{locationData.name}</div>
+          <div className="place-name">{locationData.name}</div>
           <Image src={imgUrl} alt={"image " + imgUrl} rounded fluid />
           <br />
           {locationData.description}
         </Modal.Body>
-        <Button bsPrefix="btn-branding" onClick={handleClose}>Start Exploring</Button>
+        <Button bsPrefix="btn-branding" onClick={handleClose}>
+          Start Exploring
+        </Button>
       </Modal>
     </div>
   );
