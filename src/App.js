61efccfb85a11d9e642fd96708dbc3509899da13
snowyPigeon <snoiwypigeon@yahoo.com>
2022-01-05 15:27:15 +0000

    Added leaderboard modal, and set location collect button to have dynamic text and bg-color based on user proximity

--- src/App.js
+++ src/App.js
@@ -11,12 +11,10 @@ import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App(props) {
-  // set up Mapbox credentials and map
+  // Set up Mapbox credentials and map
   const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
   const [viewport, setViewport] = useState({
-    //height: "90vh",
-    //width: "94vw",
     height: "99vh",
     width: "99vw",
     latitude: 55.952014,
@@ -25,6 +23,7 @@ export default function App(props) {
     mapboxApiAccessToken: MAPBOX_TOKEN,
   });
 
+  // Get the locations collection
   const [locations, setLocations] = useState([]);
 
   useEffect(() => {
@@ -38,8 +37,14 @@ export default function App(props) {
     return () => (mounted = false);
   }, []);
 
-  // update modal img src
+  // Update modal img src
   const [imgUrl, setImgUrl] = useState("");
+
+  // Update currently viewed location's longitude
+  const [locLng, setLocLng] = useState("");
+
+  // Update currently viewed location's latitude
+  const [locLat, setLocLat] = useState("");
 
   const navControlStyle = {
     right: 10,
@@ -88,18 +93,40 @@ export default function App(props) {
   // near Edinburgh Airport
   //const myLng = -3.370164; // REPLACE WITH USER LOCATION
 
-  // change this as needed for coordinate distance from landmark. Note 0.00001 is approx equal to 11 metres.
+  // Change this as needed for coordinate distance from landmark. Note 0.00001 is approx equal to 11 metres.
   const locTolerance = 0.00001;
 
-  // modal controls
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // Adapt the collecting button depending on user's proximity to the landmark location
+  const [collectButtonText, setCollectButtonText] = useState("Start Exploring"); // default button text
+  const [isOutOfRange, setIsOutOfRange] = useState(false);
+  const userRangeCheck = () => {
+    if (
+      locLng >= myLng - locTolerance &&
+      locLat >= myLat - locTolerance &&
+      locLng <= myLng + locTolerance &&
+      locLat <= myLat + locTolerance
+    ) {
+      setIsOutOfRange(false);
+      setCollectButtonText("Start Exploring"); // text when user in range
+    } else {
+      setIsOutOfRange(true);
+      setCollectButtonText("Please come closer to this location"); // text when user not in range
+    }
+  };
 
-  // retrieve modal data for selected pin
+  // Modal controls
+  const [showLocation, setShowLocation] = useState(false);
+  const handleCloseLocation = () => setShowLocation(false);
+  const handleShowLocation = () => setShowLocation(true);
+
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+  const handleCloseLeaderboard = () => setShowLeaderboard(false);
+  const handleShowLeaderboard = () => setShowLeaderboard(true);
+
+  // Retrieve modal data for selected pin
   const [locationData, setLocationData] = useState([]);
 
-  // retrieve city name to match modal data
+  // Retrieve city name to match modal data
   const [cityName, setCityName] = useState([]);
 
   return (
@@ -118,7 +145,10 @@ export default function App(props) {
               latitude={location.latitude}
               longitude={location.longitude}
               onClick={() => {
-                handleShow();
+                setLocLng(location.longitude);
+                setLocLat(location.latitude);
+                userRangeCheck();
+                handleShowLocation();
                 setLocationData(location);
                 if (location.photos.length > 0) {
                   setImgUrl(location.photos[0].url);
@@ -150,11 +180,13 @@ export default function App(props) {
           trackUserLocation={true}
           auto
         />
-        <Button bsPrefix="btn-leaderboard">Leaderboard</Button>
+        <Button bsPrefix="btn-leaderboard" onClick={handleShowLeaderboard}>
+          Leaderboard
+        </Button>
       </ReactMapGL>
       <Modal
-        show={show}
-        onHide={handleClose}
+        show={showLocation}
+        onHide={handleCloseLocation}
         key={locationData.id}
         scrollable
         className="location-modal"
@@ -167,7 +199,7 @@ export default function App(props) {
             fill="currentColor"
             className="bi bi-x-square closing-square"
             viewBox="0 0 16 16"
-            onClick={handleClose}
+            onClick={handleCloseLocation}
           >
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -186,9 +218,40 @@ export default function App(props) {
           <br />
           {locationData.description}
         </Modal.Body>
-        <Button bsPrefix="btn-branding" onClick={handleClose}>
-          Start Exploring
+        <Button
+          bsPrefix="btn-branding"
+          onClick={handleCloseLocation}
+          className={isOutOfRange ? 'btn-branding-out-of-range' : 'btn-branding-in-range'}
+        >
+          {collectButtonText}
         </Button>
+      </Modal>
+      <Modal
+        show={showLeaderboard}
+        onHide={handleCloseLeaderboard}
+        scrollable
+        className="leaderboard-modal"
+      >
+        <Modal.Header>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="bi bi-x-square closing-square"
+            viewBox="0 0 16 16"
+            onClick={handleCloseLeaderboard}
+          >
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>Leaderboard</h1>
+          Generic image followed by top 10 units by number of locations
+          collected.
+          <div className="units-list"></div>
+        </Modal.Body>
       </Modal>
     </div>
   );
