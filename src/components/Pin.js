809a3d089f269370697d5abf8467ae4738e458f8
Samuel Shum <55974596+samshum90@users.noreply.github.com>
2022-01-24 22:25:47 +0000

    added a pointer

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -5,7 +5,7 @@ const Pin = () => {
   return (
     <div className="pin">
       {/* styles adjusted to enlarge map pins with margins compensated */}
-      <img src={brandedMarker} alt="map pin" style={{width:"150%", height:"150%", marginLeft: "-50%", marginTop: "-50%"}}/>
+      <img src={brandedMarker} alt="map pin" style={{width:"150%", height:"150%", marginLeft: "-50%", marginTop: "-50%", cursor: "pointer"}}/>
     </div>
   );
 };
